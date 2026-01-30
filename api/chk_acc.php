07241e8c0a2aa8b3cd28417b4ce504e378c99fce
mackliu <macklun@gmail.com>
2026-01-30 15:52:22 +0800

    完成註冊功能

--- api/chk_acc.php
+++ api/chk_acc.php
@@ -1,7 +1,5 @@
 <?php include_once "db.php";
 
-$_GET['acc'];
-
 if($Mem->count(['acc'=>$_GET['acc']])>0){
     echo 1;
 }else{
--- front/regs.php
+++ front/regs.php
@@ -17,7 +17,7 @@
     <tr>
         <td class="tt ct">密碼</td>
         <td class="pp">
-            <input type="text" name="pw" id="pw">
+            <input type="password" name="pw" id="pw">
         </td>
     </tr>
     <tr>
@@ -40,7 +40,7 @@
     </tr>
  </table>
  <div class="ct">
-    <input type="submit" value="註冊">
+    <input type="button" value="註冊" onclick="regs()">
     <input type="reset" value="重置">
 </div>
 
@@ -52,6 +52,27 @@ function chkAcc(){
         alert("此帳號已存在,請重設其他帳號")
       }else{
         alert("此帳號可使用")
+      }
+    })
+}
+
+function regs(){
+    let data={acc:$("#acc").val(),
+              pw:$("#pw").val(),
+              name:$("#name").val(),
+              tel:$("#tel").val(),
+              address:$("#address").val(),
+              email:$("#email").val()
+    }
+    $.get("api/chk_acc.php",{acc:data.acc},(res)=>{
+      if(parseInt(res) || acc=='admin'){
+        alert("此帳號已存在,請重設其他帳號")
+        $("#acc").val("");
+      }else{
+        $.post("api/regs.php",data,()=>{
+            location.href='index.php?do=login';
+
+        })
       }
     })
 }
