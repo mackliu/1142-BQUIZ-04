c2a2b211620063a73bb14210e7a2aeb3c857beed
mackliu <macklun@gmail.com>
2026-01-30 15:17:56 +0800

    建置註冊登入所需頁面

---
+++ front/admin.php
@@ -0,0 +1,16 @@
+<h2>管理登入</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="text" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="chknumber" id="chknumber"></td>
+    </tr>
+</table>
+<div class="ct"><input type="submit" value="確認"></div>
--- front/regs.php
+++ front/regs.php
@@ -1,1 +1,35 @@
-註冊
+<h2 class="ct">會員註冊</h2>
+<!-- table.all>tr*6>td.tt.ct+td.pp>input:text -->
+ <table class="all">
+    <tr>
+        <td class="tt ct">姓名</td>
+        <td class="pp"><input type="text" name="name" id="name"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp">
+            <input type="text" name="acc" id="acc">
+            <input type="button" value="檢測帳號">
+        </td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="text" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電話</td>
+        <td class="pp"><input type="text" name="tel" id="tel"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">住址</td>
+        <td class="pp"><input type="text" name="address" id="address"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電子信箱</td>
+        <td class="pp"><input type="text" name="email" id="email"></td>
+    </tr>
+ </table>
+ <div class="ct">
+    <input type="submit" value="註冊">
+    <input type="reset" value="重置">
+</div>
