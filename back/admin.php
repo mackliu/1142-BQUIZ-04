5a6abafd729f57aef0e948f314409492327dd676
mackliu <macklun@gmail.com>
2026-02-02 13:54:49 +0800

    建立管理員管理相關頁面

---
+++ back/admin.php
@@ -0,0 +1,15 @@
+<div class="ct">
+    <button onclick="location.href='?do=add_admin'">新增管理員</button>  
+</div>
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="tt ct">密碼</td>
+        <td class="tt ct">管理</td>
+    </tr>
+    <tr>
+        <td class="pp ct"></td>
+        <td class="pp ct"></td>
+        <td class="pp ct"></td>
+    </tr>
+</table>
