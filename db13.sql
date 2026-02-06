7b22b8a6712880aeda5e4e72a6efb5bca68792b2
mackliu <macklun@gmail.com>
2026-02-06 16:21:31 +0800

    更新題組四資料庫

--- db13.sql
+++ db13.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2026-01-30 08:07:09
+-- 產生時間： 2026-02-06 09:20:48
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -34,6 +34,14 @@ CREATE TABLE `admin` (
   `pr` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `acc`, `pw`, `pr`) VALUES
+(1, 'admin', '1234', 'a:5:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";}'),
+(4, 'mack', '1357', 'a:3:{i:0;s:1:\"1\";i:1;s:1:\"4\";i:2;s:1:\"5\";}');
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +63,41 @@ INSERT INTO `bot` (`id`, `bot`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `no` text NOT NULL,
+  `name` text NOT NULL,
+  `price` int(10) UNSIGNED NOT NULL,
+  `spec` text NOT NULL,
+  `stock` int(10) UNSIGNED NOT NULL,
+  `img` text NOT NULL,
+  `intro` text NOT NULL,
+  `big` int(10) UNSIGNED NOT NULL,
+  `mid` int(10) UNSIGNED NOT NULL,
+  `sh` int(1) UNSIGNED NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `items`
+--
+
+INSERT INTO `items` (`id`, `no`, `name`, `price`, `spec`, `stock`, `img`, `intro`, `big`, `mid`, `sh`) VALUES
+(1, '510860', '', 0, '', 0, '0403.jpg', '', 1, 6, 1),
+(2, '020705', '兩用式磁扣腰包', 685, '中型', 18, '0404.jpg', '材質:進口牛皮\r\n顏色:黑色荔枝紋+黑色珠光面皮(黑色縫線)\r\n尺寸:15cm*14cm(高)*6cm(前後)\r\n產地:臺灣\r\n', 1, 5, 1),
+(3, '020706', '超薄設計男士長款真皮', 800, 'L號', 61, '0405.jpg', '基本:編織皮革對摺長款零錢包\r\n特色:最潮流最時尚的單品 \r\n顏色:黑色珠光面皮(黑色縫線)\r\n形狀:黑白格編織皮革對摺 ', 1, 5, 1),
+(4, '030103', '經典牛皮少女帆船鞋', 1000, 'S號', 6, '0406.jpg', '以傳統學院派風格聞名，創始近百年工藝製鞋精神\r\n共用獨家專利氣墊技術，兼具紐約工藝精神，與舒適跑格靈魂\r\n', 2, 7, 0),
+(5, '030203', '經典優雅時尚流行涼鞋', 2650, 'LL', 8, '0407.jpg', '優雅流線方型楦頭設計，結合簡潔線條綴飾，\r\n獨特的弧度與曲線美，突顯年輕優雅品味，\r\n是年輕上班族不可或缺的鞋款！\r\n全新美國運回，現貨附鞋盒', 2, 8, 1),
+(6, '040202', '寵愛天然藍寶女戒', 28000, '1克拉', 1, '0408.jpg', '◎典雅設計品味款\r\n◎藍寶為珍貴天然寶石之一，具有保值收藏\r\n◎專人設計製造，以貴重珠寶精緻鑲工製造\r\n', 3, 10, 1),
+(7, '050107', '反折式大容量手提肩背包', 888, 'L號', 15, '0409.jpg', '特色:反折式的包口設計,釘釦的裝飾,讓簡單的包型更增添趣味性\r\n材質:棉布\r\n顏色:藍色\r\n尺寸:長50cm寬20cm高41cm\r\n產地:日本\r\n', 4, 11, 0),
+(8, '060108', '男單肩包男', 650, '多功能', 7, '', '特色:男單肩包/電腦包/公文包/雙肩背包多用途\r\n材質:帆不\r\n顏色:黑色\r\n尺寸:深11cm寬42cm高33cm\r\n產地:香港\r\n', 4, 11, 0),
+(10, '204037', '木尸日火日', 12313, '日火尸木日', 12, '0410.jpg', 'dsfasdfas\r\ndsaffasf\r\nsadfasd', 1, 5, 1);
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `members`
 --
 
@@ -65,8 +108,46 @@ CREATE TABLE `members` (
   `name` text NOT NULL,
   `tel` text NOT NULL,
   `address` text NOT NULL,
-  `email` text NOT NULL
+  `email` text NOT NULL,
+  `reg_date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `members`
+--
+
+INSERT INTO `members` (`id`, `acc`, `pw`, `name`, `tel`, `address`, `email`, `reg_date`) VALUES
+(1, 'mem01', 'mem01', 'mem07', '123123123', 'dsfasdfsdaf', 'dfasdfdas@dsfasdfsa', '2026-02-02');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `types`
+--
+
+CREATE TABLE `types` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `big_id` int(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `types`
+--
+
+INSERT INTO `types` (`id`, `name`, `big_id`) VALUES
+(1, '超級流行皮件', 0),
+(2, '流行鞋區', 0),
+(3, '流行飾品', 0),
+(4, '背包', 0),
+(5, '男用皮件', 1),
+(6, '女用皮件', 1),
+(7, '少女鞋區', 2),
+(8, '紳士流行鞋區', 2),
+(9, '時尚手錶', 3),
+(10, '時尚珠寶', 3),
+(11, '背包', 4),
+(14, '', 0);
 
 --
 -- 已傾印資料表的索引
@@ -85,9 +166,21 @@ ALTER TABLE `bot`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `members`
 --
 ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `types`
+--
+ALTER TABLE `types`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -98,7 +191,7 @@ ALTER TABLE `members`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `bot`
@@ -107,10 +200,22 @@ ALTER TABLE `bot`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `types`
+--
+ALTER TABLE `types`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
