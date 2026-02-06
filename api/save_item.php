97de8330089452c5107979dff658e35fd61f31fe
mackliu <macklun@gmail.com>
2026-02-06 15:34:23 +0800

    完成新增商品功能

--- api/save_item.php
+++ api/save_item.php
@@ -1,4 +1,14 @@
 <?php include_once "db.php";
+
+
+if(!empty($_FILES['img']['tmp_name'])){
+    $_POST['img']=$_FILES['img']['name'];
+    move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_POST['img']);
+}
+
+$_POST['no']=rand(100000,999999);
+
+
 $Item->save($_POST);
 
 to("../admin.php?do=th");
