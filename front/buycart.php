4456f65a2e5c4fb937247f95531825f11a8bccad
mackliu <macklun@gmail.com>
2026-02-23 09:20:17 +0800

    建立購物車機制

--- front/buycart.php
+++ front/buycart.php
@@ -1,8 +1,23 @@
 <?php 
 
+if(isset($_GET['id'])){
+    $_SESSION['buycart'][$_GET['id']]=$_GET['qt'];
+}
+
 if(!isset($_SESSION['mem'])){
   header("location:?do=login");
   exit();
 }
+
+if(empty($_SESSION['buycart'])){
+    echo "<h2 class='ct'>購物車內沒有商品</h2>";
+}else{
+print_r($_SESSION['buycart']);
+
+
+
+}
+
+
 
 
